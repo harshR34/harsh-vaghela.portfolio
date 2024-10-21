5ea5262590afdfa3ee8b7761991c24d9041c0880
HARSH SANJAYBHAI VAGHELA <144130866+harshR34@users.noreply.github.com>
2024-10-21 10:47:58 +0530

    change file

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Project from "./components/Project";
 import Technologies from "./components/Technologies";
-import Dashboard from './components/Dashboard';
+// import Dashboard from './components/Dashboard';
 
 const App = () => {
   const [checked, setChecked] = useState(false);
@@ -26,7 +26,6 @@ const App = () => {
         <Experience />
         <Project />
         <Contact />
-        <Dashboard/>
       </div>
     </div>
   );
